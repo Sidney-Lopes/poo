ce0fad2ae44acdb1355c50121dece20795165894
Sidney Lopes <sidjlopes@yahoo.com.br>
2015-08-06 20:22:25 -0300

    - Colocando a pasta src fora do document root

--- assets/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-set_include_path( '.' . PATH_SEPARATOR . realpath( 'src' ) . DIRECTORY_SEPARATOR . PATH_SEPARATOR . get_include_path());
-
-spl_autoload_register(
-    function( $classname ){
-        require_once str_replace( '\\', DIRECTORY_SEPARATOR, $classname ) . '.php';
-});
